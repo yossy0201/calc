8cd07da61f7c632a4267427e31dfa2fb1f87064c
yossy0201 <yossy0201msk@gmail.com>
2021-02-11 23:22:24 +0900

    calc

--- calcStudy/php.php
+++ calcStudy/php.php
@@ -26,9 +26,29 @@
 
       //計算結果を表示
       
-      
+      print ($answer."\n");
     ?>
-    
+    <div id="contents">
+<form action ="php.php" method="post">
+  数字1<input type="text" class="num1" name="number1" value=""><br>
+  数字2<input type="text" class="num2" name="number2" value=""><br>
+  計算方法
+   
+   <select class="name" name="calcType">
+     <option value="plus">
+      ＋
+     </option>
+     <option value="minus">
+      −
+    </option>
+    <option value="multiplication">
+      ×
+    </option>
+   </select><br>
+   <input type="submit" class ="button" name="result" value="計算する"></button>
+   <input type = "reset" value = "クリア">
+  </form>
+  
    
   </body>
 </html>
