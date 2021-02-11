7f3562e7178f474c50e5233f3adda28b30b9ba35
yossy0201 <yossy0201msk@gmail.com>
2021-02-11 21:43:12 +0900

    calc

--- php/php.php
+++ php/php.php
@@ -1,11 +1,13 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>フォームサンプル１</title>
-</head>
-<body>
-<p><?php echo htmlspecialchars(@$_POST['namber1'], ENT_QUOTES, 'UTF-8'); ?>さん。</p>
-<p>あなたは、<?php echo (int)@$_POST['namber2']; ?> 歳です。</p>
-</body>
-</html>
+<?php
+//もし計算するボタンが押されたら
+if(isset($_POST['result'])){
+
+//足し算の時
+if($_POST["value"] === "+"){
+  $set=$_POST["number1"] + $_POST["number2"];
+  echo '<input type="text" value="'.$set.'" name="two">'; //足し算の値を表示
+  }
+
+
+
+?>
