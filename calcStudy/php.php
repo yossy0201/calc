562cc998e24398f52706287cb0faeb0b1d4ad66c
yossy0201 <yossy0201msk@gmail.com>
2021-02-11 22:26:50 +0900

    calc

---
+++ calcStudy/php.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>calc.php</title>
+  </head>
+  <body>
+    <?php
+      //値を取得
+      $number1 = $_POST['number1'];
+      $number2 = $_POST['number2'];
+      $calcType = $_POST['calcType'];
+
+      //セレクトボックスによって処理を変える
+      switch ($calcType) {
+        case "plus":
+          $answer = $number1 + $number2;
+          break;
+        case "minus":
+          $answer = $number1 - $number2;
+          break;
+        case "multiplication":
+          $answer = $number1 * $number2;
+          break;
+        default:
+          break;
+      }
+
+      //計算結果を表示
+      print ($number1." ".$calcType." ".$number1." = ".$answer."\n");
+    ?>
+    
+   
+  </body>
+</html>
