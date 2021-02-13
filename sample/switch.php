9dca7d86b976dc6e3c4da82394f4209795291f5f
yossy0201 <yossy0201msk@gmail.com>
2021-02-13 14:25:06 +0900

    switch

--- sample/switch.php
+++ sample/switch.php
@@ -3,13 +3,13 @@ $i = 0;
 
 switch ($i) {
   case 0:
-    echo "iは0です"
+    echo "iは0です";
     break;
   case 1:
-    echo "iは1です"
+    echo "iは1です";
     break;
-    default:
-    echo "iはどちらでもない"
+  default:
+    echo "iはどちらでもない";
     break;
 }
 ?>
