d65885a91872ebbec2fd5fce4ad38f3c6f3c3118
yossy0201 <yossy0201msk@gmail.com>
2021-02-13 09:33:12 +0900

    sample

--- sample/samp.php
+++ sample/samp.php
@@ -8,17 +8,17 @@ $gender = $_POST ['gender'];
 $name = $_POST ['name'];
 $title = $_POST ['title'];
 ?>
-<div class="one">性別
+<div class="one">性別:
  <?php 
   print($gender);
  ?>
 </div>
-<div class="one">年齢
+<div class="one">年齢:
  <?php 
   print($name);
  ?>才
 </div>
-<div class="one">ご要望について
+<div class="one">ご要望について:
  <?php 
   print($title);
  ?>
