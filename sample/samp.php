e7ba820545f0ec2f5e16fcd8eeab3974a44262f0
yossy0201 <yossy0201msk@gmail.com>
2021-02-13 10:18:06 +0900

    sample

--- sample/samp.php
+++ sample/samp.php
@@ -11,20 +11,43 @@ $address=$_POST['address'];
 $area = $_POST['area'];
 $title = $_POST ['title'];
 ?>
+
 <div class="one">性別:
  <?php 
   print($gender);
  ?>
 </div>
+
+<div class="one">お名前:
+ <?php 
+  print($name);
+ ?>
+</div>
+
 <div class="one">年齢:
  <?php 
   print($age);
  ?>才
 </div>
+
+<div class="one">入力した住所:
+ <?php 
+  print($address);
+ ?>
+</div>
+
+</div>
+<div class="one">選択した住所:
+ <?php 
+  print($area);
+ ?>
+</div>
+
 <div class="one">ご要望について:
  <?php 
   print($title);
  ?>
 </div>
+
   </body>
 </html>
