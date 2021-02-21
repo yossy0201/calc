c873b9e396ee8b860284a1f140a3c833427b1345
yossy0201 <yossy0201msk@gmail.com>
2021-02-21 19:04:50 +0900

    member-register

---
+++ member-register/confirm.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>メンバー登録確認</title>
+  </head>
+  <body>
+    登録名は<br>
+    <?php 
+    $name=$_POST["name"]
+    ?>になります<br>
+    よろしいですか？<br>
+    <form action="done.php" method="POST">
+    <p><input type="submit" value="はい"></p>
+    <p><input type="submit" value="いいえ"></p>
+  </form>
+  </body>
+</html>
---
+++ member-register/done.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>メンバー登録完了</title>
+  </head>
+  <body>
+    登録が完了しました。
+    <form action="topmenu.html" method="POST">
+    <p><input type="submit" value="戻る"></p>
+  </form>
+  </body>
+</html>
